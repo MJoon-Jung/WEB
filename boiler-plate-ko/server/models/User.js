42cf1d06b982d5d469d5ad16a458e1e881a49f53
MJoon-Jung <gjgjajaj31@gmail.com>
2021-06-02 20:49:11 +0900

    boiler-plate

---
+++ boiler-plate-ko/server/models/User.js
@@ -0,0 +1,97 @@
+const mongoose = require('mongoose')
+const bcryte = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcryte.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+    
+            bcryte.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+
+    //plainPassword tesetest2  암호화된 비밀번호 $2b$10$f45S196Vwk0zIiHerbjBye2r1Hjd/q3vSHAb3.u93u1/VZlT7l8cO
+
+    bcryte.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err)
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+
+    var user = this;
+    // jsonwebtoken을 이용해서 토큰 생성
+    // var token = jwt.sign(user._id, 'secretToken')
+    // -->
+    // user._id + 'secretToken' = token
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+
+    //토큰을 decode 한다.
+
+    jwt.verify(token , 'secretToken', function(err,decoded){
+    //유저 아이디를 이용해서 유저를 찾은다음에
+    //클라이언트에서 가져온 token과 보관된 token이 일치하는지 확인
+        user.findOne({"_id": decoded, "token": token}, function(err, user){
+            if(err) return cb(err);
+            cb(null, user)
+        })
+    })
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = {User}
