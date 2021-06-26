f0228a1fa882b9ec00e5c16106bf0d0730efffef
MJoon-Jung <gjgjajaj31@gmail.com>
2021-06-26 23:52:32 +0900

    redux

--- React-FullStack/FullStack-practice/front/reducers/index.js
+++ React-FullStack/FullStack-practice/front/reducers/index.js
@@ -5,13 +5,13 @@ import post from './post';
 
 const rootReducer = combineReducers({
     index: (state = {}, action) => {
-      switch (action.type) {
-        case HYDRATE:
-          console.log('HYDRATE', action);
-          return { ...state, ...action.payload };
-        default:
-          return state;
-      }
+        switch (action.type) {
+            case HYDRATE:
+                console.log('HYDRATE', action);
+                return { ...state, ...action.payload };
+            default:
+                return state;
+        }
     },
     user,
     post,
