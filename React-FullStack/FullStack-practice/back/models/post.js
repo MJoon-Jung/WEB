52833efb0e81e33e07fe8da9ee638769bfae9b30
MJoon-Jung <gjgjajaj31@gmail.com>
2021-06-30 13:30:33 +0900

    set sequelize moodel

---
+++ React-FullStack/FullStack-practice/back/models/post.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const Post = sequelize.define('Post', {
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+    }, {
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+    })
+    Post.associate = (db) => {
+        db.Post.belongsTo(db.User);
+        db.Post.hasMany(db.Comment);
+        db.Post.hasMany(db.Image);
+        db.User.belongsToMany(db.Hashtag, { through: 'PostHashtag' });
+        db.Post.belongsToMany(db.User, { through: 'Like', as: 'Likers' });
+        db.Post.belongsTo(db.Post, { as: 'Retweet' });
+    }
+}
