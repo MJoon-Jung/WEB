3dda3f7b97ab994c74d3599c413543bcf06bdfd0
MJoon-Jung <gjgjajaj31@gmail.com>
2021-07-16 08:19:55 +0900

    vue component

---
+++ vue/vue-sample/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Menubar/>
+    <JobsList :jobs= 'jobs'/>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue';
+import Menubar from './components/Menubar.vue';
+import JobsList from './components/JobsList.vue';
+import faker from 'faker';
+import Job from './types/Job';
+
+  export default defineComponent({
+    name: 'App',
+    components: {
+      Menubar,
+      JobsList,
+    },
+    setup() {
+      const jobs = ref<Job[]>([
+        { title: faker.name.jobTitle(), location: faker.random.locale(), salary: faker.random.number(), id: faker.random.uuid(), description: faker.lorem.text() },
+        { title: faker.name.jobTitle(), location: faker.random.locale(), salary: faker.random.number(), id: faker.random.uuid(), description: faker.lorem.text() },
+        { title: faker.name.jobTitle(), location: faker.random.locale(), salary: faker.random.number(), id: faker.random.uuid(), description: faker.lorem.text() },
+        { title: faker.name.jobTitle(), location: faker.random.locale(), salary: faker.random.number(), id: faker.random.uuid(), description: faker.lorem.text() },
+        { title: faker.name.jobTitle(), location: faker.random.locale(), salary: faker.random.number(), id: faker.random.uuid(), description: faker.lorem.text() },
+      ]);
+      return { jobs }
+    },
+    data() {
+    },
+    methods: {
+      
+    },
+  })
+
+</script>
