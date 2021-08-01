916af171ab5bc06debc176f17491ce87bddda5bd
MJoon-Jung <gjgjajaj31@gmail.com>
2021-08-01 23:54:04 +0900

    deleted

--- nest/todo/back/src/dto/login.request.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class LoginRequestDto {
-  @IsString()
-  name: string;
-
-  @IsString()
-  password: string;
-}
--- nest/todo/back/src/users/users.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
-
-@Controller('user')
-export class UserController {
-  constructor(private readonly userService: UsersService) {}
-}
